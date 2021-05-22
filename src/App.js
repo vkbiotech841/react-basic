7aec8e5837d84786aed3539edbccb60e234a8083
VIKRAM KUMAR <vkbiotech841@gmail.com>
2021-05-22 14:39:12 +0530

    comment added

--- src/App.js
+++ src/App.js
@@ -12,3 +12,12 @@ function App() {
 }
 
 export default App;
+
+// Here, Todo is a custome component. consider it as function nested into the App().
+// Now, consider that function accept an argument. So, here, text='Learn React' is an argument to the Todo function or component. This is way to passing data to the component in react (Props method). This type of component are called as Functional Components.
+// Now, <Todo/> component will accept these value in the Todo component and use the data for display. This data in the child component, we call as "props".  Props is an object.
+// The argument passed the functional component will be available an object (key:value) in the props.
+// Hence, properties of props can be accessed using dot notation. (eg. props.title) in the functiona component.
+// Since, properties of a props can dynamic, in react we use {} to display dynamic any javascript expression but we can not use block statement (e.g if, else) in { }.
+
+// 
--- src/components/Todo.js
+++ src/components/Todo.js
@@ -17,17 +17,45 @@ function Todo(props) {
   return (
     <div className='card'>
       <h2>{props.text}</h2>
+
       <div className='actions'>
         <button className='btn' onClick={deleteHandler}>
           Delete
         </button>
       </div>
+
       {modalIsOpen && (
         <Modal onCancel={closeModalHandler} onConfirm={closeModalHandler} />
       )}
       {modalIsOpen && <Backdrop onCancel={closeModalHandler} />}
+
     </div>
   );
 }
 
 export default Todo;
+
+// Components in react is just a function.
+// Naming function: It should start with capital character. so that react, differentiate from build component (Start with small character) from custome components (start with Capital character).
+// To turn this function into a component. This function must return a jsx.
+// To use this component into the another component, it must be exported as well.
+
+// Handling event:
+// event has trigger a function. We can write a function inside the component and then take a reference to that function on browser event inbuild functions (e.g onClick, onChange).
+
+// Why we should take a referance of the function?
+// Ans: if we are calling the function instead of taking reference of it, then as DOM element are display in the browser, that function will get executed but in actual scenario, it should get executated upon an event trigger. Hence, we write a function and then take its reference (not by calling function), when an event get triggered then that function gets executed.
+
+// useState():
+// useState registers different states of an application.
+// useState is a react hooks and it can directly called from the component function.
+// useState always an array of two elements. 
+// 1st element of the useState array is the snapshot of the current state of the application.
+// 2nd element is the function that allows to change the current state of the application.
+// In this example: modalIsOpen is the current state of the application which set as false. and setModalIsOpen is a function that can be used for changing the modalIsOpen state.
+// calling setModalIsOpen function will re-execute the entire component function and update the state of the application.
+
+// passing a function name as props:
+// in this case Modal and backdrop component just need to have onClick event. but here, the Todo.js component,we can not use onClick function direcly because they are the custome component. Hence, we need to create an argument and send as a prop to their respective componets. Then in their respective component can have onClick event. Here, closeModalHandler function reference is acting as props to the their respective components. Now, in the Modal component we need to call this function onClick. Here, we are trigger this onClick on the inbuild component (div element), hence, we are trigger click event from the child component.
+
+// In javascript functions are first class objects.you can pass then around as value. Hence we can pass a function as argument to the another function.
